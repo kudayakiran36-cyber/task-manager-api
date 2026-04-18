6c40ff7549375c237e93c51dad2de7c4a1343e9d
K Udaya Kiran <kudayakiran36@gmail.com>
2026-04-18 21:49:22 +0530

    step 1. added GET and POST test endpoints

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 from fastapi import FastAPI
 app=FastAPI()
+
 @app.get("/")
 def read_root():
     return{"message":"Hello World"}
+
+@app.get("/test")
+def get_test():
+    return{"message":"GET request working"}
+
+@app.post("/test")
+def post_test(data:dict):
+    return{"message":"POST request working",
+           "my data":data
+           }
