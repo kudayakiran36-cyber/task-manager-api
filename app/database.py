af1cc882f21ef8a40034ce41f82865fe756d90d0
K Udaya Kiran <kudayakiran36@gmail.com>
2026-04-22 16:29:33 +0530

    Switched to SQLite for deployment

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine,text
 from sqlalchemy.orm import sessionmaker,declarative_base
 from app.core.config import DATABASE_URL
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL,connect_args={"check_same_thread":False})
 SessionLocal= sessionmaker(bind=engine)
 Base= declarative_base()
 def get_db():
